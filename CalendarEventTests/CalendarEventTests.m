6a8bb0f5ad7aa95bc5738b9b940fa93d3ac47da0
Ramdhas <ramdhasios@gmail.com>
2014-05-21 16:31:35 +0530

    Initial Commit

---
+++ CalendarEventTests/CalendarEventTests.m
@@ -0,0 +1,34 @@
+//
+//  CalendarEventTests.m
+//  CalendarEventTests
+//
+//  Created by Ramdhas on May,21.
+//  Copyright (c) 2014 Ramdhas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CalendarEventTests : XCTestCase
+
+@end
+
+@implementation CalendarEventTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
