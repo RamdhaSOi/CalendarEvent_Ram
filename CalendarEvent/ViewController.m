7ab2d9ed97a134094b60e1377ec2b3a65e1c4fc5
Ramdhas <ramdhasios@gmail.com>
2014-06-17 15:32:55 +0530

    Calendar_Event_NativeCalendar App
    Add event into native calendar app and push calendar app from our app.

--- CalendarEvent/ViewController.m
+++ CalendarEvent/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <EventKitUI/EventKitUI.h>
 
 @interface ViewController ()
 
@@ -24,6 +25,56 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)AddEvent:(id)sender
+{
+    EKEventStore *eventStore=[[EKEventStore alloc] init];
+    
+    [eventStore requestAccessToEntityType:EKEntityTypeEvent completion:^(BOOL granted, NSError *error)
+     {
+         if (granted)
+         {
+             EKEvent *event  = [EKEvent eventWithEventStore:eventStore];
+             NSString * appointmentDetail =@"Ramdhas";
+             
+             NSDate *startDate = [NSDate date];
+             
+             //Create the end date components
+             NSDateComponents *tomorrowDateComponents = [[NSDateComponents alloc] init];
+             tomorrowDateComponents.day = 1;
+             
+             NSDate *endDate = [[NSCalendar currentCalendar] dateByAddingComponents:tomorrowDateComponents
+                                                                             toDate:startDate
+                                                                            options:0];
+             
+             event.title =@"Bullentin App";
+             event.startDate=startDate;
+             event.endDate=endDate;
+             event.notes = appointmentDetail;
+             // event.allDay=YES;
+             
+             [event setCalendar:[eventStore defaultCalendarForNewEvents]];
+             
+             NSError *err;
+             [eventStore saveEvent:event span:EKSpanThisEvent error:&err];
+             
+             UIAlertView * Alert = [[UIAlertView alloc]initWithTitle:nil message:@"Event added successfully" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+               [Alert show];
+             
+         }
+         else
+         {
+             NSLog(@"NoPermission to access the calendar");
+         }
+         
+     }];
+}
+
+- (IBAction)ShowCalendar:(id)sender
+{
+    NSString* launchUrl = @"calshow://";
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString: launchUrl]];
 }
 
 @end
